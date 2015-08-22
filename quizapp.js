27d33cd5fe793396a6786df289f148b91e81e932
Param Singh <paramsingh258@gmail.com>
2015-08-22 15:59:28 +0530

    Add method for updating scores

--- quizapp.js
+++ quizapp.js
@@ -1,10 +1,27 @@
 function giveObjectOption(qs, ls, n){
-    var option = {qs : qs, a: ls[0], b: ls[1], c: ls[2], d:[3], ans:ls[n-1]};
-    return option;
+    var question = {qs : qs, a: ls[0], b: ls[1], c: ls[2], d:[3], ans:ls[n-1]};
+    return question;
 }
 
 function rounds(){
-    var r1 = [giveObjectOption("Question1", "OptionA", "OptionB", "OptionC", "OptionD", 3)
-    , giveObjectOption("Question2", "OptionA", "OptionB", "OptionC", "OptionD", 2)
+    var r1 = [
+    	giveObjectOption("Question1", "OptionA", "OptionB", "OptionC", "OptionD", 3),
+    	giveObjectOption("Question2", "OptionA", "OptionB", "OptionC", "OptionD", 2)
     ]
+
+    var r3 = [
+    	giveObjectOption("Question1", "OptionA", "OptionB", "OptionC", "OptionD", 3),
+    	giveObjectOption("Question2", "OptionA", "OptionB", "OptionC", "OptionD", 2)
+    ]
+}
+
+
+ 
+function score_update(question, option_provided, team) {
+	if (question.ans === option_provided) {
+		team.score += 10;
+	}
+	else {
+	 	team.score -= 5;
+	}
 }
