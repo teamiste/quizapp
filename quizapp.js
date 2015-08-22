2504eb7c04b31e24f1cb809799abe619279108b1
Param Singh <paramsingh258@gmail.com>
2015-08-22 16:16:49 +0530

    Added a function to update questions and added a button to the page

--- quizapp.js
+++ quizapp.js
@@ -4,23 +4,26 @@ function createTeam(n){
 
 var teams = [createTeam("A"), createTeam("B"), createTeam("C"), createTeam("D"), createTeam("E")];
 
+var cur = 0;
+
 function giveObjectOption(qs, ls, n){
-    var question = {qs : qs, a: ls[0], b: ls[1], c: ls[2], d:[3], ans:ls[n-1]};
+    var question = {qs : qs, a: ls[0], b: ls[1], c: ls[2], d:ls[3], ans:ls[n-1]};
     return question;
 }
 
 function rounds(){
     var r1 = [
-    	giveObjectOption("Question1", "OptionA", "OptionB", "OptionC", "OptionD", 3),
-    	giveObjectOption("Question2", "OptionA", "OptionB", "OptionC", "OptionD", 2)
+    	giveObjectOption("Question1", ["OptionA", "OptionB", "OptionC", "OptionD"], 3),
+    	giveObjectOption("Question2", ["OptionA", "OptionB", "OptionC", "OptionD"], 2)
     ]
 
     var r3 = [
-    	giveObjectOption("Question1", "OptionA", "OptionB", "OptionC", "OptionD", 3),
-    	giveObjectOption("Question2", "OptionA", "OptionB", "OptionC", "OptionD", 2)
+    	giveObjectOption("Question1", ["OptionA", "OptionB", "OptionC", "OptionD"], 3),
+    	giveObjectOption("Question2", ["OptionA", "OptionB", "OptionC", "OptionD"], 2)
     ]
-}
 
+    return [r1, r3];
+}
 
 
 function score_update(question, option_provided, team) {
@@ -30,4 +33,12 @@ function score_update(question, option_provided, team) {
 	else {
 	 	team.score -= 5;
 	}
+}
+
+function next_question(roundNumber){
+	var r = rounds();
+	var q = r[roundNumber][cur];
+	var div = document.getElementById("div0");
+	div.innerHTML = q.qs + "<br/>" + q.a + "<br/>" + q.b + "<br/>" + q.c + "<br/>" + q.d + "<br/>" + div.innerHTML;
+	cur++;
 }
